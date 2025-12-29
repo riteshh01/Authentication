e558377b42a00baf751f4015d6e693d47db17408
Ritesh Patel <patelritesh8508@gmail.com>
2025-12-30 01:26:19 +0530

    fixed the Bug

--- server/server.js
+++ server/server.js
@@ -14,27 +14,18 @@ const allowedOrigins = [
   "https://authentication-frontend-zcls.onrender.com"
 ];
 
-
-app.use(express.json()); // ye mera middleware hai
-app.use(cookieParser()); // Cookies ko parse karta hai, Auth ke liye zaroori.
-// app.use(cors({origin: allowedOrigins, credentials: true})); // Cross-origin requests allow karta hai and credentials: true → cookies bhi allow
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (Postman, curl, server-to-server)
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error("Not allowed by CORS"));
-    }
-  },
+const corsOptions = {
+  origin: allowedOrigins,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
-}));
-app.options("*", cors());
+};
+
+app.use(express.json()); // ye mera middleware hai
+app.use(cookieParser()); // Cookies ko parse karta hai, Auth ke liye zaroori.
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 connectDB();
 
